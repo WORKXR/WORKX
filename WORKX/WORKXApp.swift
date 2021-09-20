adb16a9da62d4705dfe79013fd2ca98dd87df3e0
Cubik65536 <cubik65536@cubik65536.top>
2021-09-20 13:06:02 -0400

    Initial commit

---
+++ WORKX/WORKXApp.swift
@@ -0,0 +1,17 @@
+//
+//  WORKXApp.swift
+//  WORKX
+//
+//  Created by Cubik65536 on 2021-09-20.
+//
+
+import SwiftUI
+
+@main
+struct WORKXApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
